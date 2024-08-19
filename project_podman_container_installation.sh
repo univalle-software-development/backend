0cba505b55f26786e19d3e012a39a42d4fb0b341
Cristian David <cristian.pacheco@correounivalle.edu.co>
2024-08-19 18:05:50 -0500

    Configurating the script for the project creation

--- project_podman_container_installation.sh
+++ project_podman_container_installation.sh
@@ -11,10 +11,19 @@ podman start $container_id
 
 echo "Current directory on host: $(pwd)"
 # Stop the container after creating the archive
+
+podman exec $container_id bash -c "tar czf /tmp/archive.tar.gz -C /app/backend ."
+
 podman stop $container_id
 
 # Copy the tarball from the container to the host
-podman cp $container_id:/. .
+podman cp $container_id:/tmp/archive.tar.gz .
 
 # Clean up by removing the container
 podman rm $container_id
+
+# Extract the archive on the host
+tar -xzf archive.tar.gz
+
+# Optionally, remove the tarball after extraction
+rm archive.tar.gz
