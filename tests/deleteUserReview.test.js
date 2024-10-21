1e62deb357c50c864c5ffaed1c52753bb850ca2b
Jose Castellanos <jmcastellanos153@misena.edu.co>
2024-10-20 21:42:00 -0500

    add test for error 500

--- tests/deleteUserReview.test.js
+++ tests/deleteUserReview.test.js
@@ -64,4 +64,31 @@ describe('deleteUserReview Unit Test', () => {
     expect(res.status).toHaveBeenCalledWith(404);
     expect(res.json).toHaveBeenCalledWith({ error: 'Review not found' });
   });
+  it('should return 500 if an error occurs during the deletion process', async () => {
+    // Mock `findByPk` to throw an error
+    jest.spyOn(UserReview, 'findByPk').mockImplementation(() => {
+      throw new Error('Database error');
+    });
+    const req = {
+      params: {
+        review_id: 1,
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    await  userReviewController.deleteUserReview(req, res);
+
+    // Assert that the status 500 and error message are returned
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({ error: 'Error deleting review' });
+
+    // Restore the original implementation of `findByPk`
+    jest.restoreAllMocks();
+  });
+
+  
 });
+
